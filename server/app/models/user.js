ec42526a1b0d5dea03324c461bc0661f70ce6394
runiteict <info@runite.nl>
2015-05-29 10:49:44 +0200

    test

---
+++ server/app/models/user.js
@@ -0,0 +1,34 @@
+/*jslint node:true */
+
+(function () {
+    "use strict";
+    /**
+     * Module dependencies.
+     */
+    var mongoose = require('mongoose'),
+        Schema = mongoose.Schema,
+        schemaName,
+        modelName;
+
+	//Schema
+    schemaName = new Schema({
+        username: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        role: {type: String}
+    },
+    {collection: 'user'});
+
+    /**
+     *Validator, maybe usefull for later 
+     */
+    /*
+    schemaName.path('title').validate(function (val) {
+        return (val !== undefined && val !== null && val.length >= 8);
+    }, 'Invalid title');
+	*/
+
+	//Model
+    modelName = "User";
+    mongoose.model(modelName, schemaName);
+
+}());
